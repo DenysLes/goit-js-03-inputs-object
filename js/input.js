e4187d386f7528b336b91da3dd428dd76f4231c0
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-05-14 21:50:11 +0300

    update input.js

---
+++ js/input.js
@@ -0,0 +1,21 @@
+const shouldRenew = confirm("Бажаєте продовжити підписку?");
+console.log(shouldRenew);
+// ok=true;
+// cancel= false
+
+const quantity = prompt("Кількість товару ? ");
+console.log(quantity);
+
+// cancel=null;
+// ok= pusta stroka;
+// chyslo 5 = 5
+
+console.log(typeof quantity);
+
+// typeof=string;
+
+// let quantity = prompt("Кількість товару ? ");
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+// typeof = Number

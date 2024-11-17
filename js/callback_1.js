89c6888550e26bcb00ab8ce3d535d66c892001c2
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-11-18 00:00:03 +0200

    Update callback_1.js

--- js/callback_1.js
+++ js/callback_1.js
@@ -1,13 +1,10 @@
 /*Доповни код таким чином, щоб у змінній result був результат виконання функції makePizza, а у змінній pointer було посилання на функцію makePizza.*/
-function makePizza() {
-    return "Your pizza is being prepared, please wait.";
+function makePizza(result) {   
+  return "Your pizza is being prepared, please wait."
 }
     // Change code below this line
-
     const result = "Your pizza is being prepared, please wait.";
     const pointer = makePizza;
-    
 
-console.log(makePizza("Your pizza is being prepared, please wait."))
-// makePizza("Your pizza is being prepared, please wait.");
-
+makePizza("Your pizza is being prepared, please wait.")
+console.log(pointer);

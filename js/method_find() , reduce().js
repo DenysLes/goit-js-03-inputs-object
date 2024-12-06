cd2e4dbb41b8cbe71d62d2db50360bbfff3eaedd
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-12-06 16:19:28 +0200

    Update method_find() , reduce().js

--- js/method_find() , reduce().js
+++ js/method_find() , reduce().js
@@ -78,8 +78,82 @@
 
 
 //  Доповни функцію getSortedFriends(users) таким чином, щоб вона повертала масив унікальних імен друзів (властивість friends), відсортований за алфавітом.
-const users =
- [
+// const users =
+//  [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female"
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male"
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female"
+//   }
+// ]
+    
+// const getSortedFriends = users =>
+//   [...users].flatMap(user => user.friends).filter((friends, index, array) => array.indexOf(friends) === index).sort((a, b) => a.localeCompare(b));
+
+// console.log(getSortedFriends(users));
+
+/*Доповни функцію getTotalBalanceByGender(users, gender) таким чином, щоб вона повертала загальний баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender.*/
+
+const user =
+[
   {
     name: "Moore Hensley",
     email: "moorehensley@indexia.com",
@@ -144,8 +218,9 @@ const users =
     gender: "female"
   }
 ]
-    
-const getSortedFriends = users => 
-  [...users].flatMap(user => user.friends).filter((friends, index, array) => array.indexOf(friends) === index).sort((a, b) => a.localeCompare(b));
 
-console.log(getSortedFriends(users));
+const getTotalBalanceByGender = (users, gender) => users.filter(user => gender === user.gender).reduce((balance, item) => { return balance + item.balance }, 0);
+
+console.log(getTotalBalanceByGender());
+//✅ Якщо значення параметра `gender` - це рядок `'male'`, функція повертає число `12053`
+//✅ Якщо значення параметра `gender` - це рядок `'female'`, функція повертає число `8863`

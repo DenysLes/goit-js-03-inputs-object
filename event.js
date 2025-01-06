b4f6cdb904eae30732a63505b4529e7c0008a11b
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-01-06 18:24:12 +0200

    add event.js

--- event.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(".btn");
-
-const handleClick = (event) => {
-  console.log("event: ", event);
-  console.log("event type: ", event.type);
-  console.log("currentTarget: ", event.currentTarget);
-};
-
-button.addEventListener("click", handleClick);
